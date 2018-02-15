e95b8e388f5d11a1f146306a7af1c5177f53edaa
Stefanos.Pasios <stefanaras11@windowslive.com>
2018-02-15 22:25:38 +0200

    connect to  web app,counting num of success for every day and print the day(s) with the most chances to win

--- askisi2.py
+++ askisi2.py
@@ -42,3 +42,25 @@ hmeres = ['Deutera','Trith','Tetarth','Pempth','Paraskeuh','Savvato','Kuriakh']
 twrinhhmerom = datetime.datetime.now()
 twrinhhmeromtel = datetime.datetime.now()
 twrinhhmeromtel = twrinhhmeromtel - datetime.timedelta(days=1)
+
+for i in range (31):
+	twrinhhmerom = twrinhhmerom - datetime.timedelta(days=1)
+	date_format= twrinhhmerom.strftime("%d-%m-%Y")
+	url = 'http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json'%date_format
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	data = response.read()
+	data = json.loads(data)
+	klhrwseis = data['draws']['draw']
+	telikoplh8os = 0
+	for i in klhrwseis:
+		temp = i["results"]
+		telikoplh8os += euresiomoiwn(epiloges,temp)
+	plh8osephm.append(telikoplh8os) #h lista periexei ton ari8mo epituxiwn gia ka8e mera
+max = max(plh8osephm)
+thesi = 0
+for i in range (len(plh8osephm)): 
+	if plh8osephm[i] == max:
+		teliki = twrinhhmeromtel - datetime.timedelta(days=i) 
+		print "H h mera tou mhna me tis perissoteres epituxies einai:"
+		print hmeres[teliki.weekday()],teliki.strftime('%d-%m-%Y')
