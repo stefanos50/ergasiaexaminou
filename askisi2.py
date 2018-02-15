798a8352516efac96c3df1b50456649f90aea32c
Stefanos.Pasios <stefanaras11@windowslive.com>
2018-02-15 21:13:17 +0200

    reading 10 nums from user split by , and find current date

--- askisi2.py
+++ askisi2.py
@@ -12,3 +12,33 @@ def euresiomoiwn(x,y):
 	if plh8os2 > 4:
 		plh8os +=1
 	return plh8os
+
+plh8osephm = []
+while True:
+	flag = True
+	x = raw_input("dwste 10  ari8mous me , anamesa tous :")	
+	epiloges = x.split(",")
+	if len(epiloges)==10:
+		for i in xrange(10):
+			epiloges[i]=int(epiloges[i])
+		for i in range(0,8):
+			for j in range(i+1,10):
+				if epiloges[i] == epiloges[j]:
+					flag = False
+					print "oi ari8moi prepei na einai diaforetikoi"
+					break
+			if flag==False:
+				break
+		for i in range (0,10):
+			if epiloges[i]>80 or epiloges[i]<1:
+				flag = False
+				print 'oi ari8moi prepei na einai apo to 1-80'
+		if flag:
+			break
+	else:
+		print 'oi ari8moi prepei na einai 10'
+
+hmeres = ['Deutera','Trith','Tetarth','Pempth','Paraskeuh','Savvato','Kuriakh']
+twrinhhmerom = datetime.datetime.now()
+twrinhhmeromtel = datetime.datetime.now()
+twrinhhmeromtel = twrinhhmeromtel - datetime.timedelta(days=1)
