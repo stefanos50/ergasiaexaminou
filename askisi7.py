84f7a59ec923890c575c51c3c09b7fc89fe8c471
Stefanos.Pasios <stefanaras11@windowslive.com>
2018-02-15 19:13:55 +0200

    added final part that count the days that will apear at the same date in next 10 years

--- askisi7.py
+++ askisi7.py
@@ -7,3 +7,11 @@ currmhnas = now.month
 currday = now.day
 print "H shmerinh mera einai h",hmeres[hmeraonoma],now.strftime('%d-%m-%Y')
 print "----------"
+sum = 0
+for i in range (currxronos+1 , currxronos+11):
+	for j in range (1,13):
+		hmka8emhna = datetime.date(i ,j,currday).weekday()
+		if (hmka8emhna == hmeraonoma):
+			sum = sum + 1
+print "O arithmos apo thn mera",hmeres[hmeraonoma],"pou 8a uparxoun stis",currday,"/",currmhnas,"ta epomena 10 xronia einai:"
+print sum
