6a894e97c1808070599e509d41111b43d16ce310
Stefanos.Pasios <stefanaras11@windowslive.com>
2018-02-17 19:47:13 +0200

    added def that remove things from the tweet that we dont want and def that delete the json file if the user want to

--- askisi5.py
+++ askisi5.py
@@ -19,3 +19,26 @@ def writetweetstoajsfile(tw,flname):
 	with open(flname, 'w') as f:
 		json.dump(tw._json, f)
 		f.write('\n')
+
+li1 = ['&amp;','/','?','@','$','%',';','^','&','*','_','-',':','.','!','#','"',"'",',','(',')']
+def readtweetsfromjsfile(filename,k):
+	if filename:
+		with open(filename, 'r') as f:
+			datastore = json.load(f)
+	x = datastore['full_text'].encode('utf-8')
+	x = re.sub(r'http\S+', '', x) #removing all the url's
+	for i in range (0 , len(li1)):
+		x = re.sub(u"(\xe2\x80\x99|\xe2\x80\x9c|\xe2\x80\x9d|έΑβ)", "", x) #removing escape characters
+		x = x.replace(li1[i]," ") #removing punctuation that are in the list li1
+	tweetsli.append(x.lower().split()) #split the words where is a space
+	if k == 10:
+		return tweetsli
+def deletefileornot ():
+	while True:
+		print "Do you want to keep the json file with the data in your computer?"
+		apantisi = int(raw_input("Press 1 if YES or 0 if NO:"));
+		if apantisi == 0:
+			os.remove(usern+'.json')
+			break
+		elif apantisi == 1:
+			break
